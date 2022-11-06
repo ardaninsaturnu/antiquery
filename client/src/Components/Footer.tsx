e62035afd6188926bd6eecc2c8cad772f0dfffed
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-11-06 16:11:05 +0300

    create form input fixed. :microbe: @main

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="bg-blue-900 text-blue-100 p-5 flex justify-center">
-      <p>created by <a href="https://github.com/ardaninsaturnu">@ardaninsaturnu</a></p>
+      <p>created by <a href="https://github.com/ardaninsaturnu" className="font-medium text-blue-400">@ardaninsaturnu</a></p>
     </div>
   );
 };
--- client/src/Pages/Create/index.tsx
+++ client/src/Pages/Create/index.tsx
@@ -9,11 +9,11 @@ const Create = () => {
        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 my-5">
          <h1 className="text-3xl text-blue-500 font-medium my-2">Create new book</h1>
          <Input label="name" name="name" type="text" />
-         <Input label="name" name="name" type="text" />
-         <Input label="name" name="name" type="text" />
-         <Input label="name" name="name" type="text" />
-         <Input label="name" name="name" type="text" />
-         <Input label="name" name="name" type="text" />
+         <Input label="author name" name="author" type="text" />
+         <Input label="description" name="description" type="text" />
+         <Input label="price" name="price" type="number" />
+         <Input label="available" name="available" type="checkbox" />
+         <Input label="image" name="image" type="file" />
          <Button name="Ekle" buttonStyle='boolue' type="submit"/>
        </form>
      </div>
