b4c81089a2593f7ca46485793bf2d40ff47e3f3e
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-11-07 09:06:57 +0300

    booquery create new form created with bg. :fairy: @main

--- client/src/Pages/Create/index.tsx
+++ client/src/Pages/Create/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Input} from "../../Components/Input";
 import Button from "../../Components/Button";
+import bookcase from "../../Assets/Images/bookcase.jpg";
 
 const Create = () => {
   return (
-   <>
+    <div className="py-5" style={{background: `url(${bookcase})`, backgroundSize:'contain'}}>
      <div className="w-full max-w-xl mx-auto">
-       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 my-5">
+       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
          <h1 className="text-3xl text-blue-500 font-medium my-2">Create new book</h1>
          <Input label="name" name="name" type="text" />
          <Input label="author name" name="author" type="text" />
@@ -17,7 +18,7 @@ const Create = () => {
          <Button name="Ekle" buttonStyle='boolue' type="submit"/>
        </form>
      </div>
-   </>
+  </div>
   );
 };
 
