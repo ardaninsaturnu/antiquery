5832a9ef15c2fd7d716538f3dfe0478a620bf35c
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-11-11 22:46:37 +0300

    useform triying

--- client/src/Pages/Create/index.tsx
+++ client/src/Pages/Create/index.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import {Input} from "../../Components/Input";
 import Button from "../../Components/Button";
 import bookcase from "../../Assets/Images/bookcase.jpg";
+import {useForm} from "../../hooks/useForm";
 
 const Create = () => {
-  const handleSubmit = ( e : React.FormEvent ) => {
-      e.preventDefault();
-      console.log(e.target)
-  }
+  const { handleSubmit, handleChange, data, errors } = useForm({
+      validations: {
+        name: {
+          pattern: {
+            value: '^[A-Za-z]*$',
+            message: "You're not allowed to..."
+          },
+        },
+        author: {
+          required: {
+            value: true,
+            message: 'you must fill this field'
+          }
+        }
+      }
+  })
 
   return (
     <div className="py-5" style={{background: `url(${bookcase})`, backgroundSize:'contain'}}>
      <div className="w-full max-w-xl mx-auto">
-       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8" onSubmit={ e => handleSubmit(e)}>
+       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8">
          <h1 className="text-3xl text-blue-500 font-medium my-2">Create new book</h1>
-         <Input label="name" name="name" type="text" />
-         <Input label="author name" name="author" type="text" />
-         <Input label="description" name="description" type="text" />
-         <Input label="price" name="price" type="number" />
-         <Input label="available" name="available" type="checkbox" />
-         <Input label="image" name="image" type="file" />
-         <Button name="Ekle" buttonStyle='boolue' type="submit"/>
+
        </form>
      </div>
   </div>
@@ -28,3 +35,14 @@ const Create = () => {
 };
 
 export default Create;
+
+
+/*
+<Input label="name" name="name" type="text" />
+<Input label="author name" name="author" type="text" />
+<Input label="description" name="description" type="text" />
+<Input label="price" name="price" type="number" />
+<Input label="available" name="available" type="checkbox" />
+<Input label="image" name="image" type="file" />
+<Button name="Ekle" buttonStyle='boolue' type="submit"/>
+*/
