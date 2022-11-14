48b2a0be35d3b120e0a3d765c0c85e00e5b08663
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-11-14 07:54:01 +0300

    cors added. @main :fairy:

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from "mongoose";
 import bookRouter from "./routes/book-routes.js";
+import cors from 'cors';
 
 const app = express();
 app.use( express.json() );
+app.use( cors() );
 app.use( '/books', bookRouter );
 
 mongoose
