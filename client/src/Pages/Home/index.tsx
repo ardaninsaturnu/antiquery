b5ba129b0936b0521aa5551d03a01929e5d083d9
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-11-15 21:59:59 +0300

    fetch data typing assigned and return on page. @main :microbe:

--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -1,23 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import {Book} from "../../types";
 const URL = 'http://localhost:4000/books';
 
 const Home = () => {
-  const [ books, setBooks ] = useState<any>([])
+  const [ books, setBooks ] = useState< Book[] | null >(null );
 
   useEffect( () => {
-
     ( async () => {
       return await axios.get(URL)
         .then( res => res.data )
-        .then( data => setBooks( data ) )
+
+        .then( data => {
+          const { books } = data;
+          setBooks( books );
+
+        })
+        .catch( e => alert( e ) )
     })()
 
   },[])
 
   return (
     <>
-      {books.map( ( book : any ) => <div>{ book && book.name }</div> )}
+      { books && books.map( (a : any, index : number ) => (
+        <div key={index}>
+          <img src={a.image} width="500" alt="image"/>
+        </div>
+      ) ) }
     </>
   );
 };
---
+++ client/src/types.ts
@@ -0,0 +1,10 @@
+export interface Book {
+  _id: string;
+  name: string;
+  author: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+  __v: number;
+}
